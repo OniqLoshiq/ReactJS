e1fbc30e8736b5f26379dcb3550cd1de3adcba88
OniqLoshiq <doncho.vasilev@gmail.com>
2021-03-30 02:18:31 +0300

    Add Footer
    Create footer and fix some main css values

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,6 +1,22 @@
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0.5rem 0;
+    text-align: center;
+    background-color: blue;
+    font-size: 0.75rem;
+    color: white;
+`;
+
+
 const Footer = () => {
     return (
-        <h1>Footer </h1>
+        <Styles>
+            <span>Copyright &copy; 2021 OniqLoshiq Design since 1987. All rights reserved. </span>
+        </Styles>
     );
 };
 
