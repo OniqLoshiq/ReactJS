32f1acf51a5e821b2c470c465186559091f5d5d8
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-01 02:14:59 +0300

    Add overlay layer for Jumbotron
    Add overlay layer for Jumbotron

--- react-app/src/components/JumbotronWrapper/JumbotronWrapper.js
+++ react-app/src/components/JumbotronWrapper/JumbotronWrapper.js
@@ -16,6 +16,17 @@ const Styles = styled.div`
         text-align: center;
     }
 
+    .overlay {
+    background-color: #807e7e;
+    opacity: 0.2;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+
     .container {
        color: white
     }
@@ -26,6 +37,7 @@ const JumbotronWrapper = () => {
     return (
         <Styles>
             <Jumbotron fluid className="jumbo">
+                <div className="overlay"></div>
                 <Container>
                     <h1>Welcome</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi pariatur doloribus voluptates quas iure rerum eum accusamus eius blanditiis provident.</p>
