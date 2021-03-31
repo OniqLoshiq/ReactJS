2fbc8c4c1ab7f5eee4341d0a97de7d92ecab3412
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-01 02:15:45 +0300

    Refactor Header

--- react-app/src/components/Header/Header.js
+++ react-app/src/components/Header/Header.js
@@ -3,21 +3,56 @@ import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const Header = ({
+    shouldFormatHeader
+}) => {
+    return (
+        <Styles shouldFormat={shouldFormatHeader}>
+            <Link to='/'>
+                <img src="/EP-logo_1.png" alt="e-platform logo" />
+            </Link>
+            <Navbar expand="md" fixed="top">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Item><Link to="/">Home</Link></Nav.Item>
+                        <Nav.Item><Link to="/about">About us</Link></Nav.Item>
+                        <Nav.Item><Link to="/contacts">Contacts</Link></Nav.Item>
+                        <Nav.Item><Link to="/demo">Demo</Link></Nav.Item>
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Item><Link to="/user/profile">Hello, Dokka!</Link></Nav.Item>
+                        <Nav.Item><Link to="/user/register">Register</Link></Nav.Item>
+                        <Nav.Item><Link to="/user/signin">Sign In</Link></Nav.Item>
+                        <Nav.Item><Link to="/user/logout">Logout</Link></Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Styles>
+    );
+};
+
+export default Header;
+
 const Styles = styled.div`
     img {
         padding: 0;
         margin: 0.5rem 1rem;
         position: fixed;
-        z-index: 2000;
+        z-index: 2020;
         width: ${props => props.shouldFormat ? "40px" : "65px"};
         height: ${props => props.shouldFormat ? "40px" : "65px"};
     }
     .navbar {
-        background-color:  ${props => props.shouldFormat ? "rgba(255, 138, 101, 1)" : "rgba(255, 138, 101, .1)"};
+        background-color:  ${props => props.shouldFormat ? "rgba(255, 138, 101, 1)" : "rgba(255, 138, 101, 1)"};
         padding: 0.5rem 2rem 0.5rem 5.5rem;
         font-size: 1.1rem;
         font-weight: bold;
         justify-content: flex-end;
+        border-bottom: 0;
+        border-radius: 0 0 5px 5px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        z-index: 2010;
     }
 
     .nav-item{
@@ -40,33 +75,3 @@ const Styles = styled.div`
         }
      }
 `;
-
-const Header = ({
-    shouldFormatHeader
-}) => {
-    return (
-        <Styles shouldFormat={shouldFormatHeader}>
-            <Link to='/'>
-                <img src="/EP-logo_1.png" alt="e-platform logo" />
-            </Link>
-            <Navbar expand="md" fixed="top">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Item><Link to="/">Home</Link></Nav.Item>
-                        <Nav.Item><Link to="/about">About us</Link></Nav.Item>
-                        <Nav.Item><Link to="/contacts">Contacts</Link></Nav.Item>
-                    </Nav>
-                    <Nav className="ml-auto">
-                        <Nav.Item><Link to="/user/profile">Hello, Dokka!</Link></Nav.Item>
-                        <Nav.Item><Link to="/user/register">Register</Link></Nav.Item>
-                        <Nav.Item><Link to="/user/signin">Sign In</Link></Nav.Item>
-                        <Nav.Item><Link to="/user/logout">Logout</Link></Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </Styles>
-    );
-};
-
-export default Header;
