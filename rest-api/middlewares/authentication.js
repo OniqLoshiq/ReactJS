cd134306dee9760dd8e9c866f555ed99f9b80974
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-04 02:31:50 +0300

    Add authentication middleware

---
+++ rest-api/middlewares/authentication.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+const { COOKIE_NAME, SECRET } = require('../config');
+const BlackListToken = require('../models/BlackListToken');
+
+function verifyToken(token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, SECRET, (err, decoded) => {
+            if (err) { reject(err); return; }
+            resolve(decoded);
+        });
+    });
+}
+
+module.exports = function () {
+    return (req, res, next) => {
+        let token = req.cookies[COOKIE_NAME];
+
+        if (token) {
+            Promise.all([
+                verifyToken(token),
+                BlackListToken.findOne({ token })
+            ])
+                .then(([decoded, blackListToken]) => {
+                    if (blackListToken) { return Promise.reject(new Error('blacklisted token')) }
+                    req.user = decoded;
+
+                    return next();
+                })
+                .catch(err => {
+                    res.clearCookie(COOKIE_NAME);
+                    if (['token expired', 'blacklisted token', 'jwt must be provided'].includes(err.message)) {
+                        res.status(401).json('UNAUTHORIZED!');
+                        return;
+                    }
+                });
+        } else {
+            next();
+        }
+    }
+}
