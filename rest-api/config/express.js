b74fde3fe177067ed046c215e4dd0d7e80eb0c65
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-04 02:32:13 +0300

    Add authentication middleware to app

--- rest-api/config/express.js
+++ rest-api/config/express.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const routes = require('../routes');
+const authentication = require('../middlewares/authentication');
 
 module.exports = (app) => {
     app.use(cors());
 
     app.use(express.json());
 
+    app.use(express.urlencoded({
+        extended: true
+    }));
+
     app.use(cookieParser());
+
+    app.use(authentication());
+
+    app.use(routes);
 }
