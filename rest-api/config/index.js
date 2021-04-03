d4217cdb50a28d1e76612903dff002823ed1016a
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-04 02:30:44 +0300

    Add config values for jwt

--- rest-api/config/index.js
+++ rest-api/config/index.js
@@ -2,9 +2,15 @@ const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
     development: {
-        PORT: process.env.PORT || 5050
+        PORT: process.env.PORT || 5050,
+        SALT_ROUNDS: 10,
+        SECRET: process.env.JWT_SECRET,
+        COOKIE_NAME: 'auth-token'
     },
     production: {
-        PORT: process.env.PORT || 80
+        PORT: process.env.PORT || 80,
+        SALT_ROUNDS: 10,
+        SECRET: process.env.JWT_SECRET,
+        COOKIE_NAME: 'auth-token'
     }
 }[env]
