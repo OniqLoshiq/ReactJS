6b1c8cc08b56b7f1fd265b4a5494188eaee0c85b
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-04 02:20:50 +0300

    add mongoose option
    adding useCreateIndex due to deprecation

--- rest-api/config/mongoose.js
+++ rest-api/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
