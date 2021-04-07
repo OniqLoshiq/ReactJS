2f38e4063c75a1580af4a1af1fc229fd7d6f452b
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-07 17:05:05 +0300

    Add setState and useEffect to Jumbotron, to calculate transform property based on scroll

--- react-app/src/components/JumbotronWrapper/JumbotronWrapper.js
+++ react-app/src/components/JumbotronWrapper/JumbotronWrapper.js
@@ -1,7 +1,45 @@
+import { useState, useEffect } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import styled from 'styled-components';
 import typeWriter from '../../assets/img/typewriter.jpg'
+
+const JumbotronWrapper = () => {
+    let windowScrollTop;
+    const [transform, setTransform] = useState('');
+
+    useEffect(() => {
+        console.log(1, 'from effect')
+        if (window.innerWidth >= 768) {
+            window.addEventListener("scroll", handleTransform);
+        }
+        return function cleanup() {
+            console.log(1, 'from cleanup')
+            if (window.innerWidth >= 768) {
+                window.removeEventListener("scroll", handleTransform);
+            }
+        };
+    });
+
+    const handleTransform = () => {
+         windowScrollTop = window.pageYOffset / 3;
+        setTransform("translate3d(0," + windowScrollTop + "px,0)");
+    }
+
+    return (
+        <Styles transform={transform}>
+            <Jumbotron fluid className="jumbo">
+                <div className="overlay"></div>
+                <Container>
+                    <h1>Welcome</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi pariatur doloribus voluptates quas iure rerum eum accusamus eius blanditiis provident.</p>
+                </Container>
+            </Jumbotron>
+        </Styles>
+    );
+}
+
+export default JumbotronWrapper;
 
 const Styles = styled.div`
     .jumbo{
@@ -14,6 +52,7 @@ const Styles = styled.div`
         display: flex;
         align-items: center;
         text-align: center;
+        transform: ${props => props.transform}
     }
 
     .overlay {
@@ -30,21 +69,4 @@ const Styles = styled.div`
     .container {
        color: white
     }
-    
 `;
-
-const JumbotronWrapper = () => {
-    return (
-        <Styles>
-            <Jumbotron fluid className="jumbo">
-                <div className="overlay"></div>
-                <Container>
-                    <h1>Welcome</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi pariatur doloribus voluptates quas iure rerum eum accusamus eius blanditiis provident.</p>
-                </Container>
-            </Jumbotron>
-        </Styles>
-    );
-}
-
-export default JumbotronWrapper;
