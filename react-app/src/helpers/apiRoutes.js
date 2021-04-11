ea80d51b9908f7010cc364eae593691485d62314
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-12 01:09:04 +0300

    Add helper rest-api routes

---
+++ react-app/src/helpers/apiRoutes.js
@@ -0,0 +1,12 @@
+const url = 'http://127.0.0.1:5050/api'
+
+
+const apiRoutes = {
+    register: `${url}/users/register`,
+    singIn: `${url}/users/signIn`,
+    logout: `${url}/users/logout`,
+
+}
+
+
+export default apiRoutes;
