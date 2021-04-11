b49346350c1636d73983e55af91d1d83269ab374
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-12 01:22:44 +0300

    Update ListCategoryArticles
    Implement Card Columns

--- react-app/src/components/Article/ListCategoryArticles.js
+++ react-app/src/components/Article/ListCategoryArticles.js
@@ -1,6 +1,5 @@
 import CardColumns from 'react-bootstrap/CardColumns';
 import ListArticle from '../Article/ListArticle';
-import ListArticle2 from '../Article/ListArticle2';
 
 import styled from 'styled-components';
 
@@ -9,11 +8,14 @@ const ListCategoryArticles = () => {
         <>
             <Styles>
                 <CardColumns>
+                    <ListArticle show={true}/>
                     <ListArticle />
-                    <ListArticle2 />
-                    <ListArticle2 />
-                    <ListArticle2 />
-                    <ListArticle2 />
+                    <ListArticle show={true}/>
+                    <ListArticle />
+                    <ListArticle show={true}/>
+                    <ListArticle />
+                    <ListArticle />
+                    <ListArticle show={true}/>
                     <ListArticle />
                 </CardColumns>
             </Styles>
@@ -23,7 +25,17 @@ const ListCategoryArticles = () => {
 
 const Styles = styled.div`
     .card-columns{
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        margin: 0 auto;
+
+        -webkit-column-count: 4;
+        -moz-column-count: 4;
         column-count: 4;
+        -webkit-column-gap: 1.7rem;
+        -moz-column-gap: 1.7rem;
+        column-gap: 1.7rem;
     }
 `;
 
