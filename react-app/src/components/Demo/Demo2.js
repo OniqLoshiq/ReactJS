df4439b67c6dfd4b141ac1536291813a9030114e
OniqLoshiq <doncho.vasilev@gmail.com>
2021-04-12 23:13:03 +0300

    Replace ckeditor5 with tinymce

--- react-app/src/components/Demo/Demo2.js
+++ react-app/src/components/Demo/Demo2.js
@@ -1,43 +1,41 @@
-import React, { Component } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import { useState } from 'react'
+import { Editor } from '@tinymce/tinymce-react';
 import styled from 'styled-components';
 
-// ClassicEditor
-//     .create( document.querySelector( '#editor' ), {
-//         plugins: [ Base64UploadAdapter ]
-//     })
-//     .then(editor => {
-//         console.log( 'Editor was initialized', editor ) )
-//     .catch( );
 
-class Demo2 extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h2>Using CKEditor 5 build in React</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
-        );
+const Demo2 = () => {
+    const [content, setContent] = useState('');
+
+    const handleEditorChange = (content, editor) => {
+        setContent(content);
     }
+
+    return (
+        <Editor
+            apiKey="3ru051la3ygpszi787gxznred27xo7l4dwb1wojv9qn01k9w"
+            initialValue=""
+            init={{
+                height: 500,
+                selector: 'textarea',
+                menubar: 'edit view format tools table insert',
+                plugins: [
+                    'advlist autolink lists link image hr table charmap print preview anchor',
+                    'searchreplace visualblocks code quickbars paste media',
+                    'insertdatetime media table paste code help wordcount'
+                ],
+                toolbar:
+                    'undo redo | formatselect | bold italic backcolor hr | \
+                        alignleft aligncenter alignright alignjustify | \
+                        bullist numlist outdent indent | removeformat | image imagetools media help | \
+                        table tabledelete | tableprops tablerowprops tablecellprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol',
+                image_caption: true,
+                image_advtab: true
+
+
+            }}
+            onEditorChange={handleEditorChange}
+        />
+    );
 }
 
 const Styles = styled.div`
